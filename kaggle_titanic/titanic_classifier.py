f6b77147877122990f1c7bd784cd03a0df0c9061
Alberto Alcocer Jr <34260291+AL-JR@users.noreply.github.com>
2021-07-25 17:47:32 -0500

    Add files via upload

---
+++ kaggle_titanic/titanic_classifier.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 06:32:49 2021
+
+@author: aalco
+"""
+
+import pandas as pd
+import numpy as np
+import keras
+
+train_set = pd.read_csv("train.csv")
+test_set = pd.read_csv("test.csv")
+
